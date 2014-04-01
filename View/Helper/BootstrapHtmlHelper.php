2bca32e2827d02fc4caed976e6132616564ce377
Holt59 <capelle.mikael@gmail.com>
2014-04-01 15:32:52 +0200

    Correct bug for alert, progress bar. Change prototype of function to add shortcut (see function comments). The change are backward compatible.

--- View/Helper/BootstrapHtmlHelper.php
+++ View/Helper/BootstrapHtmlHelper.php
@@ -22,7 +22,7 @@
 
 App::import('Helper', 'Html') ;
 
-class BootstrapHtmlHelper extends HtmlHelper {    
+class Bootstrap3HtmlHelper extends HtmlHelper {    
 
     private function _extractOption ($key, $options, $default = null) {
         if (isset($options[$key])) {
@@ -34,14 +34,15 @@ class BootstrapHtmlHelper extends HtmlHelper {
     /**
      * 
      * Check type values in $options, returning null if no option is found or if
-     * option found is equal to $default or if otion is not in $avail.
+     * option is not in $avail. 
+     * If type == $default, $default is returned (even if it is not in $avail).
      * 
     **/
     private function _extractType ($options, $key = 'type', $default = 'info', 
                                       $avail = array('info', 'success', 'warning', 'error')) {
         $type = $this->_extractOption($key, $options, $default) ;
         if ($default !== false && $type == $default) {
-            return null ;
+            return $default ;
         }
         if (!in_array($type, $avail)) {
             return null ;
@@ -54,7 +55,6 @@ class BootstrapHtmlHelper extends HtmlHelper {
      * Create a Twitter Bootstrap icon.
      * 
      * @param $icon The type of the icon (search, pencil, etc.)
-     * @param $color The color of the icon (black or white)
      * 
     **/
     public function icon ($icon) {
@@ -66,15 +66,25 @@ class BootstrapHtmlHelper extends HtmlHelper {
      * Create a Twitter Bootstrap span label.
      * 
      * @param text The label text
+     * @param type The label type (default, primary, success, warning, info, danger)
      * @param options Options for span
+     * 
+     * The second parameter may either be $type or $options (in this case, the third parameter
+     * is useless, and the label type can be specified in the $options array).
      *
      * Extra options
-     *  - type The type of the label
+     *  - type The type of the label (useless if $type specified)
      *
     **/
-    public function label ($text, $options = array()) {
-        $type = $this->_extractType($options, 'type', $default = false,
-                    array('default', 'success', 'warning', 'info', 'danger')) ;
+    public function label ($text, $type = 'default', $options = array()) {
+        if (is_string($type)) {
+            $options['type'] = $type ;
+        }
+        else if (is_array($type)) {
+            $options = $type ;
+        }
+        $type = $this->_extractType($options, 'type', $default = 'default',
+                    array('default', 'primary', 'success', 'warning', 'info', 'danger')) ;
         unset ($options['type']) ;
         $options = $this->addClass($options, 'label') ;
         $options = $this->addClass($options, 'label-'.$type) ;
@@ -88,16 +98,10 @@ class BootstrapHtmlHelper extends HtmlHelper {
      * @param text The badge text
      * @param options Options for span
      *
-     * Extra options
-     *  - type The type of the badge
      *
     **/
     public function badge ($text, $options = array()) {
-        $type = $this->_extractType($options, 'type', $default = false,
-                    array('default', 'success', 'warning', 'info', 'danger')) ;
-        unset ($options['type']) ;
         $options = $this->addClass($options, 'badge') ;
-        $options = $this->addClass($options, 'badge-'.$type) ;
         return $this->tag('span', $text, $options) ;
     }
 
@@ -122,24 +126,28 @@ class BootstrapHtmlHelper extends HtmlHelper {
      * Create a Twitter Bootstrap style alert block, containing text.
      *  
      * @param $text The alert text
+     * @param $type The type of the alert
      * @param $options Options that will be passed to Html::div method
-     * 
+     *
+     * The second parameter may either be $type or $options (in this case, the third parameter
+     * is useless, and the label type can be specified in the $options array).
      * 
      * Available BootstrapHtml options:
-     * 	- block: boolean, specify if alert should have 'alert-block' class
-     * 	- type: string, type of alert (default, error, info, success)
+     * 	- type: string, type of alert (default, error, info, success ; useless if 
+     *    $type is specified)
      *     
     **/
-    public function alert ($text, $options = array()) {
-        $button = '<button class="close" data-dismiss="alert">&times;</button>' ;
-        $type = $this->_extractType($options, 'type', 'warning', array('info', 'success', 'danger')) ;
-        unset($options['type']) ;
-        $block = $this->_extractOption('block', $options, false) ;
-        unset($options['block']) ;
-        $options = $this->addClass($options, 'alert') ;
-        if ($block) {
-            $options = $this->addClass($options, 'alert-block') ;
+    public function alert ($text, $type = 'warning', $options = array()) {
+        if (is_string($type)) {
+            $options['type'] = $type ;
         }
+        else if (is_array($type)) {
+            $options = $type ;
+        }
+        $button = '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' ;
+        $type = $this->_extractType($options, 'type', 'warning', array('info', 'warning', 'success', 'danger')) ;
+        unset($options['type']) ;
+        $options = $this->addClass($options, 'alert') ;
         if ($type) {
             $options = $this->addClass($options, 'alert-'.$type) ;
         }
@@ -153,10 +161,18 @@ class BootstrapHtmlHelper extends HtmlHelper {
      * Create a Twitter Bootstrap style progress bar.
      * 
      * @param $widths 
-     * 	- The width (in %) of the bar
-     * 	- An array of bar, with width and type (info, danger, success, warning) specified for each bar
+     * 	- The width (in %) of the bar (style primary, without display)
+     * 	- An array of bar, with (for each bar) : 
+     *        - width (only field required)
+     *        - type (primary, info, danger, success, warning, default is primary)
+     *        - min (integer, default 0)
+     *        - max (integer, default 100)
+     *        - display (boolean, default false, for text display)
      * @param $options Options that will be passed to Html::div method (only for main div)
      *  
+     * If $widths is only a integer (first case), $options may contains value for the fields
+     * specified above.
+     *
      * Available BootstrapHtml options:
      * 	- striped: boolean, specify if progress bar should be striped
      * 	- active: boolean, specify if progress bar should be active
@@ -170,16 +186,37 @@ class BootstrapHtmlHelper extends HtmlHelper {
         $bars = '' ;
         if (is_array($widths)) {
             foreach ($widths as $w) {
-                $class = 'progress-bar' ;
-                $type = $this->_extractType($w, 'type', 'info', array('info', 'success', 'warning', 'danger')) ;
-                if ($type) {
-                    $class .= ' progress-bar-'.$type ;
-                }
-                $bars .= $this->div($class, '', array('style' => 'width: '.$w['width'].'%;')) ;
+                $type = $this->_extractType($w, 'type', 'primary', array('info', 'primary', 'success', 'warning', 'danger')) ;
+                $class = 'progress-bar progress-bar-'.$type ;
+                $min = $this->_extractOption('min', $w, 0);
+                $max = $this->_extractOption('max', $w, 100);
+                $display = $this->_extractOption('display', $w, false);
+                $bars .= $this->div($class, $display ? $w['width'].'%' : '', array(
+                    'aria-valuenow' => $w['width'],
+                    'aria-valuemin' => $min,
+                    'aria-valuemax' => $max,
+                    'role' => 'progressbar', 
+                    'style' => 'width: '.$w['width'].'%;'
+                )) ;
             }
         }
         else {
-            $bars = $this->div('progress-bar', '', array('style' => 'width: '.$widths.'%;')) ;
+            $type = $this->_extractType($options, 'type', 'primary', array('info', 'primary', 'success', 'warning', 'danger')) ;
+            unset($options['type']) ;
+            $class = 'progress-bar progress-bar-'.$type ;
+            $min = $this->_extractOption('min', $options, 0);
+            unset ($options['min']) ;
+            $max = $this->_extractOption('max', $options, 100);
+            unset ($options['max']) ;
+            $display = $this->_extractOption('display', $options, false);
+            unset ($options['display']) ;
+            $bars = $this->div($class, $display ? $widths.'%' : '', array(
+                'aria-valuenow' => $widths,
+                'aria-valuemin' => $min,
+                'aria-valuemax' => $max,
+                'role' => 'progressbar', 
+                'style' => 'width: '.$widths.'%;'
+            )) ;
         }
         $options = $this->addClass($options, 'progress') ;
         if ($active) {
